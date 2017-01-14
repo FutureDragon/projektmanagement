fe2b069fac17269fdae390d9dc5de6412703f93f
Thomas Emmerich <Thomas Emmerich>
2017-01-14 11:12:06 +0100

    Fix planning poker bug

--- public/javascripts/planningPokerIndex.js
+++ public/javascripts/planningPokerIndex.js
@@ -68,7 +68,7 @@ $(document).ready(function (event) {
         });
     }
 
-    $(".table").on("click", "tr", function() {
+    $(".table").on("click", "tr.click", function() {
         dialogShow.dialog( "open" );
         loadDataInForm($( this ).attr("id"));
     });
